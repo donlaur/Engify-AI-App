bbd75158e4bfc310b77097bb6ab599f915df471c
Donnie Laur <donlaur@gmail.com>
2025-10-27 14:28:59 -0400

    feat: add utils index for centralized exports

---
+++ src/lib/utils/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Utility Functions Index
+ * Central export for all utility functions
+ */
+
+// Array utilities
+export * from './array';
+
+// Date/time utilities
+export * from './date';
+
+// String utilities
+export * from './string';
+
+// Re-export cn from utils.ts for backward compatibility
+export { cn } from '../utils';
