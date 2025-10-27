4666b24f4eb1baf841ad94079348a77dad4bf14b
Donnie Laur <donlaur@gmail.com>
2025-10-27 19:55:52 -0400

    feat: add Sentry error tracking and performance monitoring

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://20caa4e4e3809a507666ec47d13d9e8c@o4510230141534208.ingest.us.sentry.io/4510264141217792',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
