26c76bbd83a28a4726e4da9cc520ae1853d02309
Donnie Laur <donlaur@gmail.com>
2025-10-27 15:03:07 -0400

    feat: add CareerContextSelector component

--- src/lib/services/UserService.ts
+++ src/lib/services/UserService.ts
@@ -1,10 +1,12 @@
 /**
  * User Service
+ * Enhanced with career context support
  */
 
 import { ObjectId } from 'mongodb';
 import { BaseService } from './BaseService';
 import { User, UserSchema } from '@/lib/db/schema';
+import { UserCareerContext, CareerLevel, CompanySize, CareerGoal } from '@/lib/types/user';
 
 export class UserService extends BaseService<User> {
   constructor() {
@@ -25,6 +27,79 @@ export class UserService extends BaseService<User> {
   async updateLastLogin(userId: string): Promise<void> {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     await this.updateOne(userId, { lastLoginAt: new Date() } as any); // Partial type
+  }
+
+  /**
+   * Update user career context
+   */
+  async updateCareerContext(
+    userId: string,
+    careerContext: Partial<UserCareerContext>
+  ): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await this.updateOne(userId, { careerContext } as any);
+  }
+
+  /**
+   * Get users by career level
+   */
+  async findByCareerLevel(level: CareerLevel): Promise<User[]> {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return this.find({ 'careerContext.level': level } as any);
+  }
+
+  /**
+   * Get users by company size
+   */
+  async findByCompanySize(size: CompanySize): Promise<User[]> {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return this.find({ 'careerContext.companySize': size } as any);
+  }
+
+  /**
+   * Get users with specific career goal
+   */
+  async findByCareerGoal(goal: CareerGoal): Promise<User[]> {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return this.find({ 'careerContext.careerGoal': goal } as any);
+  }
+
+  /**
+   * Get career context statistics
+   */
+  async getCareerStats(): Promise<{
+    byLevel: Record<CareerLevel, number>;
+    byCompanySize: Record<CompanySize, number>;
+    byGoal: Record<CareerGoal, number>;
+  }> {
+    const db = await this.getDb();
+    
+    const [byLevel, byCompanySize, byGoal] = await Promise.all([
+      db.collection(this.collectionName).aggregate([
+        { $group: { _id: '$careerContext.level', count: { $sum: 1 } } },
+      ]).toArray(),
+      db.collection(this.collectionName).aggregate([
+        { $group: { _id: '$careerContext.companySize', count: { $sum: 1 } } },
+      ]).toArray(),
+      db.collection(this.collectionName).aggregate([
+        { $group: { _id: '$careerContext.careerGoal', count: { $sum: 1 } } },
+      ]).toArray(),
+    ]);
+
+    return {
+      byLevel: byLevel.reduce((acc: any, item: any) => {
+        acc[item._id] = item.count;
+        return acc;
+      }, {}),
+      byCompanySize: byCompanySize.reduce((acc: any, item: any) => {
+        acc[item._id] = item.count;
+        return acc;
+      }, {}),
+      byGoal: byGoal.reduce((acc: any, item: any) => {
+        acc[item._id] = item.count;
+        return acc;
+      }, {}),
+    };
   }
 }
 
