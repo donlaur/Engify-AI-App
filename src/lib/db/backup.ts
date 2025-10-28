34291ae2f32b1bd9dcc0a2a4abf9dbcd1aed2224
Donnie Laur <donlaur@gmail.com>
2025-10-28 09:38:14 -0400

    feat: add backup utilities [64/500]

---
+++ src/lib/db/backup.ts
@@ -0,0 +1,9 @@
+export async function createBackup(collectionName: string) {
+  // TODO: Implement backup logic
+  console.log(`Creating backup for ${collectionName}`);
+}
+
+export async function restoreBackup(collectionName: string, backupId: string) {
+  // TODO: Implement restore logic
+  console.log(`Restoring ${collectionName} from ${backupId}`);
+}
