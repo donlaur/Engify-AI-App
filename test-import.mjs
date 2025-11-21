7e5cb411a56a38303888e5f5a5c383ae6c36863c
Donnie Laur <donlaur@gmail.com>
2025-11-21 07:00:39 -0500

    chore: remove old root-level scripts (moved to scripts/ folders)

--- test-import.mjs
+++
@@ -1,5 +0,0 @@
-// Test if ArticleRenderer can be imported
-import { ArticleRenderer } from './src/components/article/index.ts';
-
-console.log('ArticleRenderer:', ArticleRenderer);
-console.log('Type:', typeof ArticleRenderer);
